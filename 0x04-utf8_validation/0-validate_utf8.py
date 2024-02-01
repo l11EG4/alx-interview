1d047ad3fc8896568ac54717cd36f117e66808e4
l11EG4 <zakariaberahmi@gmail.com>
2024-02-01 06:26:13 -0800

    task

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Made by MEGA
+"""
+UTF-8 validation
+"""
+
+
+def validUTF8(data):
+    """
+    Data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
+    """
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,2 @@
+0x04-utf8_validation
+zakaria berahmi
